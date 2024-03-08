6b1b5bb12de93c6b30eca47cbb992935d11e71c5
ellylley <ellygou1223@gmail.com>
2024-03-08 20:18:47 +0700

    Elly

---
+++ app/Models/M_canteen.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+use CodeIgniter\Model;
+
+Class M_canteen extends Model
+{
+	public function tampil($tabel,$id){
+    return $this->db->table($tabel)
+                    ->orderby ($id,'desc') 
+                    ->get()
+                    ->getResult();
+  }
+ public function jointiga($tabel, $tabel2, $tabel3, $on, $on2, $id){
+     return $this->db->table($tabel)
+                    ->join($tabel2, $on,'left')
+                    ->join($tabel3, $on2,'left')
+                    ->orderby($id,'desc')
+                    ->get()
+                    ->getResult();
+}
+public function jointigawhere($tabel, $tabel2, $tabel3, $on, $on2, $id, $where){
+     return $this->db->table($tabel)
+                    ->join($tabel2, $on,'left')
+                    ->join($tabel3, $on2,'left')
+                    ->orderby($id,'desc')
+                    ->getWhere($where)
+                    ->getResult();
+}
+public function tambah($tabel, $isi){
+    return $this->db->table($tabel)
+                    ->insert($isi);
+  }
+  public function edit($tabel, $isi, $where){
+    return $this->db->table($tabel)
+                    ->update($isi,$where);
+  }
+  public function hapus($tabel, $where){
+    return $this->db->table($tabel)
+                    ->delete($where);
+                    
+  }
+  public function upload($photo){
+    
+        $imageName = $photo->getName();
+        $photo->move(ROOTPATH .'public/images', $imageName);
+    }	 
+
+  public function joinWhere($tabel, $tabel2, $on, $where){
+    return $this->db->table($tabel)
+            ->join($tabel2,$on,'left')
+            ->getWhere($where)
+            ->getRow();
+  }
+  public function getWhere($tabel,$where){
+    return $this->db->table($tabel)
+             ->getWhere($where)
+             ->getRow();
+             
+}
+
+}
